c8a2311b936f02bc86c532329e50e78b773e64ee
Eduardo Marin <emarin.tech@gmail.com>
2023-09-17 20:58:52 +0100

    en of lesson 106

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+function hello() {
+  console.log("Hello World!")
+}
+
+hello();
+
+// Arrow Function
+const hello_arrow = (param1) => console.log(param1)
+
+hello_arrow("Hey")
+
+const foo = {
+  num: 10,
+  logNum: () => console.log(foo.num)
+}
+
+foo.logNum()
